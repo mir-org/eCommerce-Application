30a5f92957e3dfae9cd99755215157930f37ad24
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-08-17 15:27:02 +0400

    feat: add types to form elements

--- src/app/view/main/registration/registration-view.ts
+++ src/app/view/main/registration/registration-view.ts
@@ -16,6 +16,8 @@ class RegistrationView extends View {
     this.configView();
   }
 
+  // пофиксить длину функции, вынести типы в енум
+  // eslint-disable-next-line max-lines-per-function
   configView(): void {
     const title = new ElementCreator('h1', SIGN_UP_CLASSES.TITLE, SIGN_UP_TEXT.TITLE);
     this.viewElementCreator.addInnerElement(title);
@@ -24,41 +26,54 @@ class RegistrationView extends View {
       SIGN_UP_CLASSES.REGISTRATION,
       SIGN_UP_CLASSES.FIRST_NAME,
       SIGN_UP_TEXT.FIRST_NAME,
-      SIGN_UP_KEY.FIRST_NAME
+      SIGN_UP_KEY.FIRST_NAME,
+      'text'
     );
     this.configInputFields(
       SIGN_UP_CLASSES.REGISTRATION,
       SIGN_UP_CLASSES.LAST_NAME,
       SIGN_UP_TEXT.LAST_NAME,
-      SIGN_UP_KEY.LAST_NAME
+      SIGN_UP_KEY.LAST_NAME,
+      'text'
     );
     this.configInputFields(
       SIGN_UP_CLASSES.REGISTRATION,
       SIGN_UP_CLASSES.ADDRESS,
       SIGN_UP_TEXT.ADDRESS,
-      SIGN_UP_KEY.ADDRESS
+      SIGN_UP_KEY.ADDRESS,
+      'text'
     );
-    this.configInputFields(SIGN_UP_CLASSES.REGISTRATION, SIGN_UP_CLASSES.EMAIL, SIGN_UP_TEXT.EMAIL, SIGN_UP_KEY.EMAIL);
+    this.configInputFields(
+      SIGN_UP_CLASSES.REGISTRATION,
+      SIGN_UP_CLASSES.EMAIL,
+      SIGN_UP_TEXT.EMAIL,
+      SIGN_UP_KEY.EMAIL,
+      'email'
+    );
     this.configInputFields(
       SIGN_UP_CLASSES.REGISTRATION,
       SIGN_UP_CLASSES.PASSWORD,
       SIGN_UP_TEXT.PASSWORD,
-      SIGN_UP_KEY.PASSWORD
+      SIGN_UP_KEY.PASSWORD,
+      'password'
     );
     this.configInputFields(
       SIGN_UP_CLASSES.REGISTRATION,
       SIGN_UP_CLASSES.CONFIRM_PASSWORD,
       SIGN_UP_TEXT.CONFIRM_PASSWORD,
-      SIGN_UP_KEY.CONFIRM_PASSWORD
+      SIGN_UP_KEY.CONFIRM_PASSWORD,
+      'password'
     );
     const submitBtn = new ElementCreator('button', SIGN_UP_CLASSES.BUTTON, SIGN_UP_TEXT.BUTTON);
+    submitBtn.getElement().setAttribute('type', 'submit');
     this.form.addInnerElement(submitBtn);
   }
 
-  configInputFields(firstClass: string, secondClass: string, text: string, key: string) {
+  configInputFields(firstClass: string, secondClass: string, text: string, key: string, type: string) {
     const inputField = new InputFieldsCreator(firstClass, secondClass, text, this.state.getValue(key), (event) =>
       this.keyupHandler(event, key)
     );
+    inputField.setType(type);
     this.form.addInnerElement(inputField.getElement());
   }
 
