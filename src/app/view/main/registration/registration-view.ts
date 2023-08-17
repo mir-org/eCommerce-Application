d419090d16863eda9211078bc3de070b57d2d610
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-08-17 15:17:33 +0400

    refactor: add form element

--- src/app/view/main/registration/registration-view.ts
+++ src/app/view/main/registration/registration-view.ts
@@ -7,15 +7,19 @@ import { SIGN_UP_CLASSES, SIGN_UP_TEXT, SIGN_UP_KEY } from '../../../enums/enums
 class RegistrationView extends View {
   state: State;
 
+  form: HTMLFormElement | ElementCreator;
+
   constructor(state: State) {
     super('section', SIGN_UP_CLASSES.REGISTRATION);
     this.state = state;
+    this.form = new ElementCreator('form', 'registration-form');
     this.configView();
   }
 
   configView(): void {
     const title = new ElementCreator('h1', SIGN_UP_CLASSES.TITLE, SIGN_UP_TEXT.TITLE);
     this.viewElementCreator.addInnerElement(title);
+    this.viewElementCreator.addInnerElement(this.form);
     this.configInputFields(
       SIGN_UP_CLASSES.REGISTRATION,
       SIGN_UP_CLASSES.FIRST_NAME,
@@ -48,14 +52,14 @@ class RegistrationView extends View {
       SIGN_UP_KEY.CONFIRM_PASSWORD
     );
     const submitBtn = new ElementCreator('button', SIGN_UP_CLASSES.BUTTON, SIGN_UP_TEXT.BUTTON);
-    this.viewElementCreator.addInnerElement(submitBtn);
+    this.form.addInnerElement(submitBtn);
   }
 
   configInputFields(firstClass: string, secondClass: string, text: string, key: string) {
     const inputField = new InputFieldsCreator(firstClass, secondClass, text, this.state.getValue(key), (event) =>
       this.keyupHandler(event, key)
     );
-    this.viewElementCreator.addInnerElement(inputField.getElement());
+    this.form.addInnerElement(inputField.getElement());
   }
 
   keyupHandler(event: KeyboardEvent, fieldName: string) {
