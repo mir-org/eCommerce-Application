1d402aab5465dc5ed98bdfc0f1fdbe1f7255bd77
toronto-tokyo <93839077+toronto-tokyo@users.noreply.github.com>
2023-08-18 20:58:41 +0200

    5.3.fix api (#11)
    ## PR Type
    
    What kind of change does this PR introduce?
    
    ```
    - [ ] Feature
    - [x] Bugfix
    - [ ] Refactoring
    - [ ] Documentation changes
    - [ ] Tests
    - [ ] Other
    ```
    
    ## What's new?
    
    Make API's method static and refuse fetchRefreshToken methos

--- src/api/authAPI/authAPI.ts
+++ src/api/authAPI/authAPI.ts
@@ -1,13 +1,13 @@
 import { TOKEN_STORAGE_KEY, CTP_AUTH_URL, CTP_PROJECT_KEY, CTP_CLIENT_ID, CTP_CLIENT_SECRET } from '../api-data';
 
 export class AuthAPI {
-  public async setAccessToken(): Promise<void> {
+  public static async setAccessToken(): Promise<void> {
     if (!localStorage.getItem(TOKEN_STORAGE_KEY)) {
       await this.fetchAnonymousToken();
     }
   }
 
-  private async fetchAnonymousToken(): Promise<void> {
+  private static async fetchAnonymousToken(): Promise<void> {
     const url = `${CTP_AUTH_URL}/oauth/${CTP_PROJECT_KEY}/anonymous/token?grant_type=client_credentials`;
     const response = await fetch(url, {
       method: 'POST',
@@ -20,7 +20,7 @@ export class AuthAPI {
     localStorage.setItem(TOKEN_STORAGE_KEY, accessToken);
   }
 
-  public async fetchPasswordToken(email: string, password: string): Promise<void> {
+  public static async fetchPasswordToken(email: string, password: string): Promise<void> {
     const url = `${CTP_AUTH_URL}/oauth/${CTP_PROJECT_KEY}/customers/token?grant_type=password&username=${email}&password=${password}`;
     const response = await fetch(url, {
       method: 'POST',
@@ -29,11 +29,11 @@ export class AuthAPI {
       },
     });
     const data = await response.json();
-    const refreshToken = data.refresh_token;
-    await this.fetchRefreshToken(refreshToken);
+    const accessToken = data.access_token;
+    localStorage.setItem(TOKEN_STORAGE_KEY, accessToken);
   }
 
-  private async fetchRefreshToken(token: string): Promise<void> {
+  private static async fetchRefreshToken(token: string): Promise<void> {
     const url = `${CTP_AUTH_URL}/oauth/token?grant_type=refresh_token&refresh_token=${token}`;
     const response = await fetch(url, {
       method: 'POST',
