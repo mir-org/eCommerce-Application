5764502f3bd6d1f2cca5a0421d20406639384437
Minchuk Ilya <cacatubutyt@gmail.com>
2023-08-19 16:58:20 +0200

    feat: add login page

---
+++ src/app/view/main/login/login-view-types.ts
@@ -0,0 +1,37 @@
+export const CssClasses = {
+  TITLE: 'login__title',
+  LOGIN: 'login',
+  ERROR_LINE: 'login_error-line',
+  FORM: 'login__form',
+  EMAIL_INPUT: 'email',
+  PASSWORD_INPUT: 'password',
+  LOGIN_BUTTON: ['login__button', 'primary-button'],
+};
+
+export const TEXT = {
+  TITLE: 'LOGIN',
+  ERROR_LINE: 'Incorrect username or password',
+  FORM: '',
+  EMAIL_INPUT: 'Ел.адрес',
+  PASSWORD_INPUT: 'Пароль',
+  BUTTON: 'Войти',
+};
+
+export const TYPE = {
+  INPUT_TYPE: {
+    EMAIL: 'email',
+    PASSWORD: 'password',
+  },
+  BUTTON_TYPE: 'submit',
+};
+
+export const INITIAL_VALUE = {
+  INPUT_VALUE: {
+    EMAIL: '',
+    PASSWORD: '',
+  },
+  PLACEHOLDER: {
+    EMAIL: '',
+    PASSWORD: '',
+  },
+};
--- src/app/view/main/login/login-view.ts
+++ src/app/view/main/login/login-view.ts
@@ -1,59 +1,118 @@
-import State from '../../../state/state';
+import { CustomerAPI } from '../../../../api/CustomerAPI/CustomerAPI';
+import { StatusCodes } from '../../../../api/CustomerAPI/customer-api-type';
+import { AuthAPI } from '../../../../api/authAPI/authAPI';
+import { Pages } from '../../../router/pages';
+import { Router } from '../../../router/router';
 import { ElementCreator } from '../../../utils/element-creator';
 import InputFieldsCreator from '../../../utils/input-fields-creator';
 import { View } from '../../view';
-
-const CssClasses = {
-  LOGIN: 'login',
-  TITLE: 'login__title',
-  EMAIL: 'email',
-  PASSWORD: 'password',
-};
-
-const TEXT = {
-  FIELD_TEXT_ONE: 'Ваш мейл',
-  FIELD_TEXT_TWO: 'Ваш пароль',
-  TITLE: 'ЭТО СТРАНИЦА АВТОРИЗАЦИИ',
-};
-
-const KEY_FOR_SAVE = {
-  email: 'login__email-input',
-  password: 'login__password-input',
-};
+import { CssClasses, INITIAL_VALUE, TEXT, TYPE } from './login-view-types';
 
 class LoginView extends View {
-  private state: State;
+  private form: ElementCreator | null;
 
-  constructor(state: State) {
+  private errorLine: HTMLElement | null;
+
+  private emailInput: HTMLInputElement | null;
+
+  private passwordInput: HTMLInputElement | null;
+
+  constructor(private router: Router) {
     super('section', CssClasses.LOGIN);
-    this.state = state;
+    this.form = null;
+    this.errorLine = null;
+    this.emailInput = null;
+    this.passwordInput = null;
     this.configView();
   }
 
   private configView(): void {
-    const title = new ElementCreator('h1', CssClasses.TITLE, TEXT.TITLE);
-    this.viewElementCreator.addInnerElement(title);
-    const emailInput = new InputFieldsCreator(
-      CssClasses.LOGIN,
-      CssClasses.EMAIL,
-      TEXT.FIELD_TEXT_ONE,
-      this.state.getValue(KEY_FOR_SAVE.email),
-      (event) => this.keyupHandler(event, KEY_FOR_SAVE.email)
-    );
-    this.viewElementCreator.addInnerElement(emailInput.getElement());
-    const passwordInput = new InputFieldsCreator(
-      CssClasses.LOGIN,
-      CssClasses.PASSWORD,
-      TEXT.FIELD_TEXT_TWO,
-      this.state.getValue(KEY_FOR_SAVE.password),
-      (event) => this.keyupHandler(event, KEY_FOR_SAVE.password)
-    );
-    this.viewElementCreator.addInnerElement(passwordInput.getElement());
+    this.addTitle();
+    this.addErrorLine();
+    this.addForm();
+    this.configForm();
   }
 
-  private keyupHandler(event: KeyboardEvent, fieldName: string): void {
-    if (event.target instanceof HTMLInputElement) {
-      this.state.setValue(fieldName, event.target.value);
+  private configForm(): void {
+    this.addEmailInput();
+    this.addPasswordInput();
+    this.addLoginButton();
+  }
+
+  private addTitle(): void {
+    const titleCreator = new ElementCreator('h1', CssClasses.TITLE, TEXT.TITLE);
+    this.viewElementCreator.addInnerElement(titleCreator);
+  }
+
+  private addErrorLine(): void {
+    const errorLineCreator = new ElementCreator('div', CssClasses.ERROR_LINE, TEXT.ERROR_LINE);
+    const errorLineElement = errorLineCreator.getElement();
+    this.errorLine = errorLineElement;
+    this.viewElementCreator.addInnerElement(errorLineElement);
+  }
+
+  private addForm(): void {
+    const formCreator = new ElementCreator('form', CssClasses.FORM, TEXT.FORM);
+    this.form = formCreator;
+    this.viewElementCreator.addInnerElement(formCreator.getElement());
+  }
+
+  private addEmailInput(): void {
+    const emailInputCreator = new InputFieldsCreator(
+      CssClasses.LOGIN,
+      CssClasses.EMAIL_INPUT,
+      TEXT.EMAIL_INPUT,
+      INITIAL_VALUE.INPUT_VALUE.EMAIL,
+      TYPE.INPUT_TYPE.EMAIL,
+      INITIAL_VALUE.PLACEHOLDER.EMAIL
+    );
+    const emailInputElement = emailInputCreator.getInputElement();
+    this.emailInput = emailInputElement;
+    emailInputElement.addEventListener('keydown', this.inputKeydownFn.bind(this));
+    this.form?.addInnerElement(emailInputCreator.getElement());
+  }
+
+  private addPasswordInput(): void {
+    const passwordInputCreator = new InputFieldsCreator(
+      CssClasses.LOGIN,
+      CssClasses.PASSWORD_INPUT,
+      TEXT.PASSWORD_INPUT,
+      INITIAL_VALUE.INPUT_VALUE.PASSWORD,
+      TYPE.INPUT_TYPE.PASSWORD,
+      INITIAL_VALUE.PLACEHOLDER.PASSWORD
+    );
+    const passwordInputElement = passwordInputCreator.getInputElement();
+    this.passwordInput = passwordInputElement;
+    passwordInputElement.addEventListener('keydown', this.inputKeydownFn.bind(this));
+    this.form?.addInnerElement(passwordInputCreator.getElement());
+  }
+
+  private addLoginButton(): void {
+    const loginButtonCreator = new ElementCreator('button', CssClasses.LOGIN_BUTTON, TEXT.BUTTON);
+    const loginButtonElement = loginButtonCreator.getElement();
+    loginButtonElement.setAttribute('type', TYPE.BUTTON_TYPE);
+    loginButtonElement.addEventListener('click', (e) => this.loginButtonClickFn.call(this, e));
+    this.form?.addInnerElement(loginButtonElement);
+  }
+
+  private inputKeydownFn(): void {
+    this.errorLine?.classList.remove('show');
+  }
+
+  private async loginButtonClickFn(event: Event): Promise<void> {
+    if (!this.emailInput || !this.passwordInput) {
+      throw new Error();
+    }
+    event.preventDefault();
+    const email = this.emailInput.value;
+    const password = this.passwordInput.value;
+    const loginStatusCode = await CustomerAPI.loginCustomer(email, password);
+    if (loginStatusCode === StatusCodes.successfulLogin) {
+      await AuthAPI.fetchPasswordToken(email, password);
+      this.router.navigate(Pages.INDEX);
+      await CustomerAPI.getCustomerInfo();
+    } else {
+      this.errorLine?.classList.add('show');
     }
   }
 }
