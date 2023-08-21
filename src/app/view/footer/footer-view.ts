a1f263f1fbf0318978380d535f4b362d38bf1bf6
miroslav-zarenkov <60261308+miroslav-zarenkov@users.noreply.github.com>
2023-08-22 01:27:44 +0400

    2.1.1.add basic 404 and footer blocks (#18)
    ## PR Type
    
    What kind of change does this PR introduce?
    
    ```
    - [x] Feature
    - [ ] Bugfix
    - [ ] Refactoring
    - [ ] Documentation changes
    - [ ] Tests
    - [ ] Other
    ```
    
    ## What's new?
    
    Add basic 404 error page and style footer.
    
    Co-authored-by: Ginezis <106539249+GinezisNWD@users.noreply.github.com>

--- src/app/view/footer/footer-view.ts
+++ src/app/view/footer/footer-view.ts
@@ -1,11 +1,25 @@
+import { ElementCreator } from '../../utils/element-creator';
 import { View } from '../view';
-
-const CssClasses = 'footer';
-const TEXT = 'SPA';
 
 class FooterView extends View {
   constructor() {
-    super('footer', CssClasses, TEXT);
+    super('footer', 'footer');
+    this.configView();
+  }
+
+  private configView(): void {
+    const footerRssLink = new ElementCreator('a', 'footer__link', '');
+    footerRssLink.getElement().setAttribute('href', 'https://rs.school/js/');
+    footerRssLink.getElement().setAttribute('target', '_blank');
+    footerRssLink.getElement().setAttribute('rel', 'noopener noreferrer');
+    this.viewElementCreator.addInnerElement(footerRssLink);
+    const rssImage = new ElementCreator('img', 'footer__link-image', '');
+    rssImage.getElement().setAttribute('src', '../../../../assets/images/rs_school_js.svg');
+    footerRssLink.addInnerElement(rssImage);
+    const teamText = new ElementCreator('div', 'footer__text-team', 'Developed by New World Disorder');
+    this.viewElementCreator.addInnerElement(teamText);
+    const yearText = new ElementCreator('div', 'footer__text-year', '2023');
+    this.viewElementCreator.addInnerElement(yearText);
   }
 }
 
