aab075a699cc56207ef18fc672aa192c1b67fc81
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-08-28 17:42:46 +0400

    feat: add basic page

---
+++ src/app/view/main/catalog/catalog-view-types.ts
@@ -0,0 +1,13 @@
+export const CATALOG_CLASSES = {
+  CATALOG: 'catalog',
+  TITLE: 'catalog__title',
+  CONTENT: 'catalog__content',
+  ASIDE: 'catalog__aside',
+  PRODUCTS: 'catalog__products',
+};
+export const CATALOG_TEXT = {
+  TITLE: 'Catalog',
+  CONTENT: '',
+  ASIDE: 'Aside',
+  PRODUCTS: 'Products',
+};
--- src/app/view/main/catalog/catalog-view.ts
+++ src/app/view/main/catalog/catalog-view.ts
@@ -1,13 +1,43 @@
 import { View } from '../../view';
 import { Router } from '../../../router/router';
-
-const CssClasses = {
-  CATALOG: 'catalog',
-};
+import { ElementCreator } from '../../../utils/element-creator';
+import { CATALOG_CLASSES, CATALOG_TEXT } from './catalog-view-types';
 
 class CatalogView extends View {
+  private content: ElementCreator | null;
+
   constructor(private router: Router) {
-    super('section', CssClasses.CATALOG);
+    super('section', CATALOG_CLASSES.CATALOG);
+    this.content = null;
+    this.configView();
+  }
+
+  private configView(): void {
+    this.addTitle();
+    this.addContent();
+    this.addAside();
+    this.addProducts();
+  }
+
+  private addTitle(): void {
+    const title = new ElementCreator('h1', CATALOG_CLASSES.TITLE, CATALOG_TEXT.TITLE);
+    this.viewElementCreator.addInnerElement(title);
+  }
+
+  private addContent(): void {
+    const content = new ElementCreator('div', CATALOG_CLASSES.CONTENT, CATALOG_TEXT.CONTENT);
+    this.content = content;
+    this.viewElementCreator.addInnerElement(content);
+  }
+
+  private addAside(): void {
+    const aside = new ElementCreator('aside', CATALOG_CLASSES.ASIDE, CATALOG_TEXT.ASIDE);
+    this.content?.addInnerElement(aside);
+  }
+
+  private addProducts(): void {
+    const products = new ElementCreator('div', CATALOG_CLASSES.PRODUCTS, CATALOG_TEXT.PRODUCTS);
+    this.content?.addInnerElement(products);
   }
 }
 
