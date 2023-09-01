d650668b91fd0fa17e0945d500d20a1c19658c2d
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-09-02 01:39:19 +0400

    feat: add basic page and user data

---
+++ src/app/view/main/user-profile/user-profile-types.ts
@@ -0,0 +1,8 @@
+export const PROFILE_CLASSES = {
+  PROFILE: 'user-profile',
+  TITLE: 'user-profile__title',
+};
+
+export const PROFILE_TEXT = {
+  TITLE: 'Profile',
+};
--- src/app/view/main/user-profile/user-profile.ts
+++ src/app/view/main/user-profile/user-profile.ts
@@ -1,13 +1,31 @@
+import { CustomerAPI } from '../../../../api/customer-api/customer-api';
+import { ElementCreator } from '../../../utils/element-creator';
 import { View } from '../../view';
+import { PROFILE_CLASSES, PROFILE_TEXT } from './user-profile-types';
 
 class UserProfileView extends View {
+  private form: ElementCreator | null;
+
   constructor() {
-    super('section', 'user-profile');
+    super('section', PROFILE_CLASSES.PROFILE);
+    this.form = null;
     this.configView();
   }
 
   private configView(): void {
-    console.log('user-profile');
+    this.addTitle();
+    this.addForm();
+    console.log(CustomerAPI.getCustomerInfo());
+  }
+
+  private addTitle(): void {
+    const title = new ElementCreator('h1', PROFILE_CLASSES.TITLE, PROFILE_TEXT.TITLE);
+    this.viewElementCreator.addInnerElement(title);
+  }
+
+  private addForm(): void {
+    this.form = new ElementCreator('form', 'registration__form');
+    this.viewElementCreator.addInnerElement(this.form.getElement());
   }
 }
 
