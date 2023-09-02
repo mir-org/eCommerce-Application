205566071b6837660457060fdfda6aa4e4b30af9
Ginezis <Ginezis25@gmail.com>
2023-09-02 23:10:15 +0300

    refactor: remove useles trash

--- src/app/router/history-router-handler.ts
+++ src/app/router/history-router-handler.ts
@@ -10,21 +10,15 @@ type RouterHandlerParam = {
 };
 
 class HistoryRouterHandler {
-  public params: RouterHandlerParam;
-
   public callback: (result: RequestParams) => void;
 
   public handler: (url: PopStateEvent | string) => void;
 
   constructor(callback: (requestParams: RequestParams) => void) {
-    this.params = {
-      nameEvent: 'popstate',
-      locationField: 'pathname',
-    };
     this.callback = callback;
     this.handler = this.navigate.bind(this);
 
-    if (this.params.nameEvent === 'popstate') window.addEventListener(this.params.nameEvent, this.handler);
+    window.addEventListener('popstate', this.handler);
   }
   public navigate(url: PopStateEvent | string): void {
     if (typeof url === 'string') {
